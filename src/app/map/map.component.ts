f7cef9dce7c7ccea6445cc3b38d1da2b1c2d3498
Marco Volken <marco_volken@bluewin.ch>
2022-04-26 00:06:53 +0200

    WIP

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,8 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
 import * as D3 from 'd3';
 import * as TopoJSON from 'topojson-client';
-import { GeometryCollection } from 'topojson-specification';
+import { Topology, GeometryCollection } from 'topojson-specification';
 import Gemeindeverzeichnis from '../../assets/gemeindeverzeichnis.json';
+
+// TODO: Kantone einfärben mit leichtem Gradient in Farben des Kantonswappens
+// TODO: Auswahl welche Routen angezeigt werden sollen (national, regional, lokal, alle) über UI
 
 @Component({
     selector: 'app-map',
@@ -13,9 +16,6 @@ import Gemeindeverzeichnis from '../../assets/gemeindeverzeichnis.json';
     encapsulation: ViewEncapsulation.None
 })
 export class MapComponent implements OnInit {
-
-    // @ViewChild('map')
-    // mapRef: ElementRef
 
     private svg: D3.Selection<SVGSVGElement, unknown, HTMLElement, any>
     private width: number
@@ -56,13 +56,18 @@ export class MapComponent implements OnInit {
             .attr('width', this.width)
             .attr('height', this.height)
             .attr('class', 'background')
-            .on('click', (e: PointerEvent) => {
-                D3.selectAll('.municipality-boundary').classed('active', false)
-                D3.selectAll('.canton-boundary').classed('active', false)
+            .on('click', () => {
+                D3.selectAll('.municipality').classed('active', false)
+                // D3.selectAll('.canton').classed('active', false)
+                D3.selectAll('.route').classed('active', false)
+                D3.selectAll('.route-endpoint').remove()
             })
 
         this.svg.append('g')
             .attr('class', 'country')
+
+        this.svg.append('g')
+            .attr('class', 'lakes')
 
         this.svg.append('g')
             .attr('class', 'municipalities')
@@ -74,19 +79,19 @@ export class MapComponent implements OnInit {
             .attr('class', 'routes')
 
         this.svg.append('g')
-            .attr('class', 'foreground')
+            .attr('class', 'locations')
 
         // Define pan and zoom behaviour
-        const zoomBehaviour = D3.zoom<SVGSVGElement, unknown>()
+        const zoomBehaviour: D3.ZoomBehavior<any, any> = D3.zoom()
             .scaleExtent(this.zoomExtent)
-            .on('zoom', e => {   
-                this.zoomTransform = e.transform
+            .on('zoom', (event: D3.D3ZoomEvent<any, any>) => {   
+                this.zoomTransform = event.transform
                 D3.selectAll('path')
-                    .attr('transform', e.transform)
+                    .attr('transform', event.transform.toString())
                 D3.selectAll('circle')
-                    .attr('transform', (d: any) => e.transform + `translate(${this.projection([d[0], d[1]])})`)
-                    .attr('r', 8 / e.transform.k) 
-                    .attr('style', `stroke-width: ${1 / e.transform.k}`)
+                    .attr('transform', (datum: any) => event.transform.toString() + `translate(${this.projection([datum[0], datum[1]])})`)
+                    .attr('r', 8 / event.transform.k) 
+                    .attr('style', `stroke-width: ${1 / event.transform.k}`)
             })
             
         this.svg.call(zoomBehaviour)
@@ -98,14 +103,14 @@ export class MapComponent implements OnInit {
             this.projection
                 .scale((this.width + this.height / 2) * this.projectionScale)
                 .translate([this.width / 2, this.height / 2])
-            this.svg
+            this.svg 
                 .attr('width', this.width)
                 .attr('height', this.height)
             D3.selectAll<SVGPathElement, any>('path')
                 .attr('d', this.path)
             D3.selectAll('circle')
-                .attr('transform', (d: any) => this.zoomTransform + `translate(${this.projection([d[0],d[1]])})`)
-                .attr('r', 4 / this.zoomTransform.k) 
+                .attr('transform', (datum: any) => this.zoomTransform.toString() + `translate(${this.projection([datum[0], datum[1]])})`)
+                .attr('r', 8 / this.zoomTransform.k) 
                 .attr('style', `stroke-width: ${1 / this.zoomTransform.k}`)
         }
 
@@ -113,8 +118,9 @@ export class MapComponent implements OnInit {
 
     private async renderAsync(): Promise<void> {
 
-        const topology: any = await D3.json('./assets/topologie.json')
-        const routes: any = await D3.json('./assets/wanderland/kombiniert.json')
+        // Load topoloy and routes
+        const topology: Topology = (await D3.json('./assets/topologie/kombiniert.json')) as Topology
+        const routes: Topology = (await D3.json('./assets/wanderland/kombiniert.json')) as Topology
 
         // Initialize projection and path to match topology
         this.projection = D3.geoMercator()
@@ -132,37 +138,44 @@ export class MapComponent implements OnInit {
             .append('path')
             .attr('d', this.path)
 
+        // Render lakes
+        D3.select('.lakes').selectAll('path')
+            .data(TopoJSON.feature(topology, topology.objects.lakes as GeometryCollection).features)
+            .enter()
+            .append('path')
+            .attr('d', this.path)
+            .attr('class', datum => `lake l${datum.id}`)
+            .on('mouseenter', (event: MouseEvent, datum: any) => {
+                console.warn(datum)
+            })
+
         // Render municipalities
         D3.select('.municipalities').selectAll('path')
             .data(TopoJSON.feature(topology, topology.objects.municipalities as GeometryCollection).features)
             .enter()
             .append('path')
             .attr('d', this.path)
-            .attr('class', d => `municipality-boundary m${d.id}`)
-            .on('mouseover', function(e: PointerEvent, d) {
-                console.warn(Gemeindeverzeichnis.GDE.find(gde => gde.GDENR === d.id)?.GDENAME)
-                D3.selectAll('.municipality-boundary').classed('active', false)
-                D3.select(this).classed('active', true)
+            .attr('class', datum => `municipality m${datum.id}`)
+            .on('mouseenter', (event: MouseEvent, datum: any) => {
+                const municipalityName = Gemeindeverzeichnis.GDE.find(gemeinde => gemeinde.GDENR === datum.id)?.GDENAME
+                const cantonName = Gemeindeverzeichnis.GDE.find(gemeinde => gemeinde.GDENR === datum.id)?.GDEKTNA
+                console.warn(municipalityName, cantonName)
             })
-
-        // Render cantons    
+     
+        // Render cantons
         D3.select('.cantons').selectAll('path')
             .data(TopoJSON.feature(topology, topology.objects.cantons as GeometryCollection).features)
             .enter()
             .append('path')
             .attr('d', this.path)
-            .attr('class', d => `canton-boundary c${d.id}`)
-            .on('click', function(e: PointerEvent, d) {
-                console.warn(Gemeindeverzeichnis.KT.find(kt => kt.KTNR === d.id)?.GDEKT)
-                D3.selectAll('.municipality-boundary').classed('active', false)
-                D3.selectAll('.canton-boundary').classed('active', false)
-                D3.select(this).classed('active', true)
-                // const lonLat = this.projection.invert!([e.pageX, e.pageY])
-                // console.warn(lonLat![1], lonLat![0])                    
-            })
-            .on('mouseout', function(e: PointerEvent, d) {
-                D3.selectAll('.municipality-boundary').classed('active', false)                  
-            })
+            .attr('class', (datum: any) => `canton c${datum.id}`)
+            // .on('click', (event: PointerEvent, datum: any) => {
+            //     console.warn(Gemeindeverzeichnis.KT.find(kt => kt.KTNR === datum.id)?.GDEKT)
+            //     D3.selectAll('.canton').classed('active', false)
+            //     D3.select(event.target as Element).classed('active', true).raise()
+            //     // const lonLat = this.projection.invert!([e.pageX, e.pageY])
+            //     // console.warn(lonLat![1], lonLat![0])                    
+            // })
 
         // Render routes    
         D3.select('.routes').selectAll('path')
@@ -171,46 +184,74 @@ export class MapComponent implements OnInit {
             .append('path')
             .attr('d', this.path)
             .attr('class', 'route')
-            .on('click', (e: PointerEvent, d: any) => {
-                console.warn(d.properties)
+            .on('click', (event: PointerEvent, datum: any) => {
+                console.warn(datum.properties)
                 D3.selectAll('.route').classed('active', false)
-                D3.select(e.target as any).classed('active', true).raise()
-                this.renderSelectedRouteEndpoints(d)
+                D3.select(event.target as any).classed('active', true).raise()
+                this.renderSelectedRouteEndpoints(datum)
+            })
+            .on('mouseenter', (event: MouseEvent) => {
+                D3.select(event.target as Element).raise()
+            })
+            .on('mouseout', (event: MouseEvent) => {
+                if (!D3.select(event.target as Element).classed('active')) {
+                    D3.select(event.target as Element).lower()
+                }
             })
 
         // Render locations
-        D3.select('.foreground').selectAll('circle')
+        D3.select('.locations').selectAll('circle')
             .data(this.locations)
             .enter()
             .append('circle')
             .attr('r', 8)
-            .attr('transform', (d: any) => `translate(${this.projection([d[0], d[1]])})`)
+            .attr('transform', (datum: any) => `translate(${this.projection([datum[0], datum[1]])})`)
             .attr('class', 'location')
-            .on('click', (e: PointerEvent, d) => {
-                console.warn(d)
+            .on('click', (event: PointerEvent, datum: any) => {
+                console.warn(datum)
             })
 
     }
 
-    private renderSelectedRouteEndpoints(dRoute: any): void {
+    private renderSelectedRouteEndpoints(routeDatum: any): void {
+
+        const projectedStartPoint = this.projection(routeDatum.geometry.coordinates[0])
+        const projectedEndPoint = this.projection(routeDatum.geometry.coordinates[routeDatum.geometry.coordinates.length - 1])
+
+        const svgStartPoint = this.svg.node()!.createSVGPoint()
+        svgStartPoint.x = projectedStartPoint![0]
+        svgStartPoint.y = projectedStartPoint![1]
+
+        const svgEndPoint = this.svg.node()!.createSVGPoint()
+        svgEndPoint.x = projectedEndPoint![0]
+        svgEndPoint.y = projectedEndPoint![1]
+
+        D3.selectAll('.municipality')
+            .classed('active', (_, index: number, nodes: any) => nodes[index].isPointInFill(svgStartPoint) || nodes[index].isPointInFill(svgEndPoint))
+        D3.selectAll('.municipality.active')
+            .raise()
         D3.select('.routes').selectAll('circle').remove()
         D3.select('.routes').selectAll('circle')
-            .data([dRoute.geometry.coordinates[0], dRoute.geometry.coordinates[dRoute.geometry.coordinates.length - 1]])
+            .data([routeDatum.geometry.coordinates[0], routeDatum.geometry.coordinates[routeDatum.geometry.coordinates.length - 1]])
             .enter()
             .append('circle')
-            .attr('transform', (d: any) => this.zoomTransform + `translate(${this.projection([d[0], d[1]])})`)
+            .attr('transform', (datum: any) => this.zoomTransform.toString() + `translate(${this.projection([datum[0], datum[1]])})`)
             .attr('r', 8 / this.zoomTransform.k)
             .attr('style', `stroke-width: ${1 / this.zoomTransform.k}`)
             .attr('class', 'route-endpoint')
-            .on('click', (e: PointerEvent, dEndpoint) => {
-                console.warn(dEndpoint)
+            .on('click', (event: PointerEvent, datum: any) => {
+                console.warn(datum)
             })
+
     }
 
     // private async fetchDataAsync(): Promise<void> {
+    //
     //     const path = 'https://api3.geo.admin.ch/rest/services/api/MapServer'
     //     const response = await lastValueFrom(this.http.get(path))
+    //
     //     console.warn(response)
+    //
     // }
 
 }
