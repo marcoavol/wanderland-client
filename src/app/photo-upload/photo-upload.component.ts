58cc43155645583b016dd37772dcd048cf482e0a
Marco Volken <marco_volken@bluewin.ch>
2022-05-25 17:36:02 +0200

    WIP

--- src/app/photo-upload/photo-upload.component.ts
+++ src/app/photo-upload/photo-upload.component.ts
@@ -1,22 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-photo-upload',
-  templateUrl: './photo-upload.component.html',
-  styleUrls: ['./photo-upload.component.scss']
+    selector: 'app-photo-upload',
+    templateUrl: './photo-upload.component.html',
+    styleUrls: ['./photo-upload.component.scss']
 })
 export class PhotoUploadComponent implements OnInit {
 
-  constructor(
-      private modalService: NgbModal,
-  ) { }
+    public photo: File | null
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private modalService: NgbModal,
+    ) { }
 
-  openModal(content: any) {
-    this.modalService.open(content, { centered: true })
-  }
+    ngOnInit(): void {
+    }
+ 
+    public openModal(content: any) {
+        this.modalService.open(content, { centered: true })
+    }
+
+    public handlePhotoInput(event: Event): void {
+        const files = (event.target as HTMLInputElement).files
+        this.photo = files?.length ? files[0] : null
+    }
+
+    public submit(event: Event): void {
+        event.preventDefault()
+        if (this.photo) {
+            const formData = new FormData()
+            formData.append(this.photo.name, this.photo, this.photo.name)
+        }
+        console.warn(this.photo)
+    }
 
 }
