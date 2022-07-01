5a94b17b62564a33fe755d6ba6355bf902fc69ec
jb <jan.birkhofer@bluewin.ch>
2022-07-01 09:25:34 +0200

    add unit tests for nav-bar component

---
+++ src/app/home/nav-bar/nav-bar.component.spec.ts
@@ -0,0 +1,64 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NavBarComponent } from './nav-bar.component';
+import { MapSettingsService } from '../map/map-settings.service';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+
+fdescribe('NavBarComponent', () => {
+
+    let component: NavBarComponent
+    let fixture: ComponentFixture<NavBarComponent>
+    let mapSettingsService: MapSettingsService
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [NavBarComponent],
+            imports: [
+                NgbTypeaheadModule
+            ],  
+        }).compileComponents();       
+    }))
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(NavBarComponent)
+        component = fixture.componentInstance
+        fixture.detectChanges()
+    
+        mapSettingsService = TestBed.inject(MapSettingsService)
+        mapSettingsService.currentSettings = { ...mapSettingsService['DEFAULT_SETTINGS'] }
+    })
+
+    it('should be created', () => {
+        expect(component).toBeTruthy()
+        expect(mapSettingsService).toBeTruthy()
+    })
+
+       
+    it('should search for canton', () => {
+        let cantonName = "Glarus"
+        const submitEvent = new Event('submit')
+
+        component.searchInput.nativeElement.value = cantonName
+        component.searchByName(submitEvent)
+        expect(mapSettingsService.currentSettings.cantonId).toBe(8)
+
+        cantonName = "Schaffhausen"
+        component.searchInput.nativeElement.value = cantonName
+        component.searchByName(submitEvent)
+        expect(mapSettingsService.currentSettings.cantonId).toBe(14)
+
+        cantonName = "Zürich"
+        component.searchInput.nativeElement.value = cantonName
+        component.searchByName(submitEvent)
+        expect(mapSettingsService.currentSettings.cantonId).toBe(1)
+    })
+
+    it('should reset search method called', () => {
+        const cantonName = "Zug"
+        component.searchInput.nativeElement.value = cantonName
+        expect(component.searchInput.nativeElement.value).toBe(cantonName)
+        component.resetSearchInput()
+        expect(mapSettingsService.currentSettings.cantonId).toBe(undefined)
+        expect(component.searchInput.nativeElement.value).toBe("")
+    })
+
+});
--- src/app/utils/unit-utils.service.spec.ts
+++ src/app/utils/unit-utils.service.spec.ts
@@ -13,20 +13,30 @@ fdescribe('UnitUtilsService', () => {
     expect(service).toBeTruthy();
   })
 
-  it('convert to kilometers', () => {
+  it('should convert to kilometers', () => {
     expect(service.convertToUnitString(0, 'Kilometers')).toBe("0")
     expect(service.convertToUnitString(50000, 'Kilometers')).toBe("50")
     expect(service.convertToUnitString(1000000, 'Kilometers')).toBe("1000")
+    expect(service.convertToUnitString(1450200, 'Kilometers')).toBe("1450.2")
   })
 
-  it('convert to days hours and minutes', () => {
+  it('should convert to kilometers => formatWithUnit enabled', () => {
+    expect(service.convertToUnitString(0, 'Kilometers', true)).toBe("0.0km")
+    expect(service.convertToUnitString(50500, 'Kilometers', true)).toBe("50.5km")
+    expect(service.convertToUnitString(1450200, 'Kilometers', true)).toBe("1450.2km")
+    expect(service.convertToUnitString(765250, 'Kilometers', true)).toBe("765.3km")
+    expect(service.convertToUnitString(3245, 'Kilometers', true)).toBe("3.2km")
+    expect(service.convertToUnitString(3251, 'Kilometers', true)).toBe("3.3km")
+  })
+
+  it('should convert to days hours and minutes', () => {
     expect(service.convertToUnitString(15, 'DaysHoursMinutes')).toBe("00:15")
     expect(service.convertToUnitString(1440, 'DaysHoursMinutes')).toBe("01:00:00")
     expect(service.convertToUnitString(333, 'DaysHoursMinutes')).toBe("05:33")
     expect(service.convertToUnitString(7932, 'DaysHoursMinutes')).toBe("05:12:12")
   })
 
-  it('convert to days hours and minutes => formatWithUnit enabled', () => {
+  it('should convert to days hours and minutes => formatWithUnit enabled', () => {
     expect(service.convertToUnitString(15, 'DaysHoursMinutes', true)).toBe("0h 15m")
     expect(service.convertToUnitString(1440, 'DaysHoursMinutes', true)).toBe("1d 0h 0m")
     expect(service.convertToUnitString(333, 'DaysHoursMinutes', true)).toBe("5h 33m")
